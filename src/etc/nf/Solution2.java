1c5dfde4664a9b074cda749bdc837c93c79eeb5a
yeahajeong <yeahajeong@daum.net>
2021-02-26 00:30:22 +0900

    원안에 들어갈 수 있는지 판단하는 알고리즘 [실패]
    나에게는 너무 어려운 문제였다. 어떤식으로 풀어야할지 여러 아이디어를 생각하였지만 어떤 케이스는 해결되고 어떤 케이스는 해결되지 않는 그런 상황이 발생했다. 결국 해결하지 못함..ㅠㅠ 내일 새마음가짐으로 다시 풀어보도록 하자.

---
+++ src/etc/nf/Solution2.java
@@ -0,0 +1,15 @@
+package etc.nf;
+
+/* 문제 해결 아이디어
+* 1. x와 y가 범위 안에 있으면 카운트하기 -> 실패
+* 2. x와 y의 차를 이용해서 카운트하기 -> 실패
+* */
+
+public class Solution2 {
+
+  public int solution(int inner, int outer, int[] points_x, int[] points_y) {
+    int answer = 0;
+    return answer;
+  }
+
+}
