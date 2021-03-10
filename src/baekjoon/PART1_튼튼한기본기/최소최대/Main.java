cc51a6004c322053d17e33d5efe965b84bf6f875
yeahajeong <yeahajeong@daum.net>
2021-03-10 21:27:44 +0900

    백준 - 최대최소

--- src/baekjoon/PART1_튼튼한기본기/최소최대/Main.java
+++ src/baekjoon/PART1_튼튼한기본기/최소최대/Main.java
@@ -5,7 +5,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
 /*
@@ -21,53 +20,67 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-  public static void solution1() {
-    //입력 - Scanner 사용
-    // 선언 방식이 쉽고 간단, 다양한 자료형을 입력값으로 저장 가능
-    // next() : 공백 이전까지의 문자를 입력받음
-    // nextLine() : 개행 전까지의 문자열 전체를 입력받음 -> 한줄씩 입력받음
-    // 버퍼의 길이가 1024로 한정적, 한번에 많은 입력값을 저장하는데 비효율적
+  public static void solution1() throws IOException {
+    /* Scanner vs BufferedReader 비교
+    - Scanner 사용
+    선언 방식이 쉽고 간단, 다양b한 자료형을 입력값으로 저장 가능
+    next() : 공백 이전까지의 문자를 입력받음
+    nextLine() : 개행 전까지의 문자열 전체를 입력받음 -> 한줄씩 입력받음
+    버퍼의 길이가 1024로 한정적, 한번에 많은 입력값을 저장하는데 비효율적
+
     Scanner scan = new Scanner(System.in);
     int N = scan.nextInt(); //몇개의 정수를 받을 건지
-
     //배열
     int[] array = new int[N];
-
     for (int i = 0; i < N; i++) {
       array[i] = scan.nextInt();
     }
-
     scan.close();
-    Arrays.sort(array); //정렬 - O(n^2)
 
-    System.out.println(array[0] + " " + array[N-1]);
-  }
-
-  public static void solution2() throws IOException {
-    //입력 - BufferedReader 사용
-    //버퍼의 길이가 scanner보다 크고 많은 데이터를 한 번에 받아오고자 할 때 효율적
-    //String 타입만 받고 다른 자료형을 사용하고싶다면 추가변환 or 파싱 필요
-    //개행 기준으로 입력받기 때문에 공백기준으로 받는 경우 별도 파싱필요
-    //IOException 처리해줘야함
+    - BufferedReader 사용
+    버퍼의 길이가 scanner보다 크고 많은 데이터를 한 번에 받아오고자 할 때 효율적
+    String 타입만 받고 다른 자료형을 사용하고싶다면 추가변환 or 파싱 필요
+    개행 기준으로 입력받기 때문에 공백기준으로 받는 경우 별도 파싱필요
+    IOException 처리해줘야함
+    */
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int N = Integer.parseInt(br.readLine());
 
     // 공백도 같이 입력되니까 분리를 해준다.
-    StringTokenizer st = new StringTokenizer(br.readLine(), "");
+    StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
     int[] array = new int[N];
     int i = 0;
-    while (!st.hasMoreTokens()) {
+    while (st.hasMoreTokens()) {
       array[i] = Integer.parseInt(st.nextToken()); //다음 토큰을 반환
       i++;
     }
 
-    Arrays.sort(array);
+    Arrays.sort(array); //최대 O(n^2)
     System.out.println(array[0] + " " + array[N-1]);
   }
 
+  // 바로바로 비교하는 풀이
+  public static void solution2() throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
+
+    StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+    int min = 1000000;
+    int max = -1000000;
+
+    // O(n)
+    while (st.hasMoreTokens()) {
+      int temp = Integer.parseInt(st.nextToken());
+      if (min > temp) min = temp;
+      if (max < temp) max = temp;
+    }
+
+    System.out.println(min + " " + max);
+  }
+
   public static void main(String[] args) throws IOException {
-    solution1();
     solution2();
   }
 
